68ab4db5a8d6b4ad0f032ee57d8b1faf13d14fe4
Dr Wave <dr@worldwidewaves.net>
2024-08-09 05:32:26 +0200

    fix issue with jq usage

--- scripts/maps/libs/lib.inc.sh
+++ scripts/maps/libs/lib.inc.sh
@@ -1,18 +1,18 @@
 # Events configuration file
 EVENTS_FILE=../../shared/src/commonMain/composeResources/files/events.json
 
-# List of all configured events
-EVENTS=$(jq -r '.[].id' $EVENTS_FILE)
-
 # Download jq for JSON decoding with bash
 mkdir -p ./bin
 [ ! -f ./bin/jq ] && wget -np -q https://github.com/stedolan/jq/releases/latest/download/jq-linux64 -O ./bin/jq && chmod +x ./bin/jq
+
+# List of all configured events
+EVENTS=$(./bin/jq -r --arg unknown "unknown" '.[] | select(.mapBbox != $unknown) | .id' $EVENTS_FILE)
 
 # ----------
 
 function conf() { # Read event configuration property value
                   # CALL: conf $event $prop
-  jq -r --arg event "$1" --arg prop "$2" '.[] | select(.id == $event) | .[$prop]' $EVENTS_FILE
+  ./bin/jq -r --arg event "$1" --arg prop "$2" '.[] | select(.id == $event) | .[$prop]' $EVENTS_FILE
 }
 
 # ----------
@@ -22,7 +22,7 @@ function tpl() { # Replace event configuration values in template file
                  # CALL: tpl $event $template_file $output_file
   TPL=$(mktemp)
   cp $2 $TPL
-  for prop in $(jq -r --arg event "$1" '.[] | select(.id == $event) | keys[]' $EVENTS_FILE); do
+  for prop in $(./bin/jq -r --arg event "$1" '.[] | select(.id == $event) | keys[]' $EVENTS_FILE); do
     TMP=$(mktemp)
     cat $TPL | sed -e 's/#'$prop'#/'"$(conf $1 $prop | sed -e 's/\//\\\//g')"'/g' > $TMP
     rm -f $TPL
