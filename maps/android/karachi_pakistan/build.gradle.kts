a4c45764e7c09adedb2aac404be28a5748780aa5
Dr Wave <dr@worldwidewaves.net>
2025-06-26 22:12:55 +0200

    added karachi

---
+++ maps/android/karachi_pakistan/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    alias(libs.plugins.android.dynamic.feature)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+android {
+    namespace = "com.worldwidewaves.maps.karachi_pakistan"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    implementation(project(":composeApp"))
+}
