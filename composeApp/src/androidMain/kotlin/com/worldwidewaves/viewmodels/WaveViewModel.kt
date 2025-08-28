f5d0ce4d10bd068d74434bfd89e42ef1d80da101
Dr Wave <dr@worldwidewaves.net>
2025-08-28 11:57:50 +0200

    Updated Copyright

--- composeApp/src/androidMain/kotlin/com/worldwidewaves/viewmodels/WaveViewModel.kt
+++ composeApp/src/androidMain/kotlin/com/worldwidewaves/viewmodels/WaveViewModel.kt
@@ -1,7 +1,7 @@
 package com.worldwidewaves.viewmodels
 
 /*
- * Copyright 2024 DrWave
+ * Copyright 2025 DrWave
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- shared/src/commonTest/kotlin/com/worldwidewaves/shared/events/WWWEventWaveLinearTest.kt
+++ shared/src/commonTest/kotlin/com/worldwidewaves/shared/events/WWWEventWaveLinearTest.kt
@@ -1,7 +1,7 @@
 package com.worldwidewaves.shared.events
 
 /*
- * Copyright 2024 DrWave
+ * Copyright 2025 DrWave
  *
  * WorldWideWaves is an ephemeral mobile app designed to orchestrate human waves through cities and
  * countries, culminating in a global wave. The project aims to transcend physical and cultural
