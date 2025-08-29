016cc58141df44cb5eee70176930a299576c0156
Dr Wave <dr@worldwidewaves.net>
2025-08-29 12:48:39 +0200

    updated event FAQ

--- composeApp/src/androidMain/kotlin/com/worldwidewaves/compose/tabs/about/AboutInfoScreen.kt
+++ composeApp/src/androidMain/kotlin/com/worldwidewaves/compose/tabs/about/AboutInfoScreen.kt
@@ -21,45 +21,30 @@ package com.worldwidewaves.compose.tabs.about
  * limitations under the License.
  */
 
-import android.util.Log
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalUriHandler
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import com.worldwidewaves.activities.utils.TabScreen
 import com.worldwidewaves.compose.tabs.AboutDividerLine
 import com.worldwidewaves.compose.tabs.AboutWWWLogo
 import com.worldwidewaves.compose.tabs.AboutWWWSocialNetworks
-import com.worldwidewaves.shared.WWWGlobals.Companion.DIM_DEFAULT_INT_PADDING
 import com.worldwidewaves.shared.WWWGlobals.Companion.DIM_DEFAULT_SPACER_BIG
 import com.worldwidewaves.shared.WWWGlobals.Companion.DIM_DEFAULT_SPACER_SMALL
 import com.worldwidewaves.shared.WWWGlobals.Companion.DIM_INFO_DRWAVE_FONTSIZE
-import com.worldwidewaves.shared.WWWGlobals.Companion.DIM_INFO_DRWAVE_INSTA_FONTSIZE
 import com.worldwidewaves.shared.WWWGlobals.Companion.DIM_INFO_TEXT_FONTSIZE
 import com.worldwidewaves.shared.generated.resources.drwave
-import com.worldwidewaves.shared.generated.resources.drwave_instagram
-import com.worldwidewaves.shared.generated.resources.drwave_instagram_url
 import com.worldwidewaves.shared.generated.resources.infos_core
-import com.worldwidewaves.shared.generated.resources.instagram_icon
 import com.worldwidewaves.theme.commonJustifiedTextStyle
-import com.worldwidewaves.theme.commonTextStyle
 import com.worldwidewaves.theme.extraBoldTextStyle
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import com.worldwidewaves.shared.generated.resources.Res as ShRes
 
