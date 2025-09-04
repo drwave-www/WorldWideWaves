7dc453c6ac99d6b341cbba1066b51e7f751d0c31
Dr Wave <dr@worldwidewaves.net>
2025-09-04 23:24:21 +0200

    chore: Increase version number to 14/v0.10

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -53,8 +53,8 @@ android {
         applicationId = "com.worldwidewaves"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 13
-        versionName = "v0.9"
+        versionCode = 14
+        versionName = "v0.10"
         ndk {
             // Ship only the arm64-v8a ABI to minimise download size
             abiFilters += listOf("arm64-v8a")
