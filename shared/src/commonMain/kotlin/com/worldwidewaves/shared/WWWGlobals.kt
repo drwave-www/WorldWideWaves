311e2d949208f32a1935a86904acf0b37f07f145
Dr Wave <dr@worldwidewaves.net>
2025-09-07 02:54:47 +0200

    Updated headers

--- shared/src/commonMain/kotlin/com/worldwidewaves/shared/WWWGlobals.kt
+++ shared/src/commonMain/kotlin/com/worldwidewaves/shared/WWWGlobals.kt
@@ -12,7 +12,7 @@ import kotlin.time.Duration.Companion.seconds
  * Copyright 2025 DrWave
  *
  * WorldWideWaves is an ephemeral mobile app designed to orchestrate human waves through cities and
- * countries, culminating in a global wave. The project aims to transcend physical and cultural
+ * countries. The project aims to transcend physical and cultural
  * boundaries, fostering unity, community, and shared human experience by leveraging real-time
  * coordination and location-based services.
  *
