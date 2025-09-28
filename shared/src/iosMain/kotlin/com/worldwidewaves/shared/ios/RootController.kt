e1da5205e0dd33e35c5c716c7625bf9b4222f9c7
Dr Wave <dr@worldwidewaves.net>
2025-09-28 21:25:16 +0200

    CLEAN COMPILE - BUILD.GRADLE VERIFIED - HELLO FROM COMPOSE

--- shared/src/iosMain/kotlin/com/worldwidewaves/shared/ios/RootController.kt
+++ shared/src/iosMain/kotlin/com/worldwidewaves/shared/ios/RootController.kt
@@ -1,7 +1,14 @@
 package com.worldwidewaves.shared.ios
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.ComposeUIViewController
 import platform.Foundation.NSLog
 import platform.UIKit.UIViewController
@@ -14,6 +21,17 @@ fun AppUI() {
 
 fun MakeMainViewController(): UIViewController =
     ComposeUIViewController(configure = { enforceStrictPlistSanityCheck = false }) {
-        NSLog(">>> COMPOSE CONTENT LAMBDA ENTER")
-        AppUI()
+        NSLog(">>> COMPOSE CONTENT LAMBDA ENTER - 2")
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFFFFD54F)), // visible amber background
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Hello from Compose (iOS)",
+                color = Color.Black,
+                fontSize = 28.sp
+            )
+        }
     }
