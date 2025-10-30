f48bbf325a2fa122207132015582396ab3d1bd86
Dr Wave <dr@worldwidewaves.net>
2025-10-31 00:44:17 +0100

    Android to build 35 / version v1.0

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -112,7 +112,7 @@ android {
             libs.versions.android.targetSdk
                 .get()
                 .toInt()
-        versionCode = 34
+        versionCode = 35
         versionName = "v1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
